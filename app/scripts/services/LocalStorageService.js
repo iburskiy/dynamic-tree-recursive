0ebd6c966b1c1bcba60fe03fa675536cae0d9546
iburskyy <iburskyy@exadel.com>
2018-03-09 19:17:38 +0300

    Factories are corrected to be services (according to AngularJS ideology)

--- app/scripts/services/LocalStorageService.js
+++ app/scripts/services/LocalStorageService.js
@@ -1,32 +1,27 @@
 'use strict';
 
-angular.module( 'dynamic-tree-recursive' ).factory('localStorageService', ['CONSTANTS', function(CONSTANTS) {
+angular.module( 'dynamic-tree-recursive' ).service('localStorageService', ['CONSTANTS', function(CONSTANTS) {
 
-    return {
-        //Saves tree to Local Storage
-        save: function ( tree ) {
-            var treeJson = angular.toJson( tree );
-            localStorage.setItem(CONSTANTS.TREE_STATE, treeJson);
-            console.log('The following tree has beed saved: ' + treeJson);
-        },
-
-        //Reads tree from Local Storage
-        retrieve: function () {
-            var retrievedObject = localStorage.getItem(CONSTANTS.TREE_STATE);
-            return JSON.parse(retrievedObject);
-        },
-
-        //Deletes tree from Local Storage
-        delete: function () {
-            localStorage.removeItem(CONSTANTS.TREE_STATE);
-        },
-
-        //Checks if tree is saved in Local Storage
-        isTreeSaved: function () {
-            if (localStorage.getItem(CONSTANTS.TREE_STATE) === null) {
-                return false;
-            }
-            return true;
-        }
+    //Saves tree to Local Storage
+    this.save = function ( tree ) {
+      var treeJson = angular.toJson( tree );
+      localStorage.setItem(CONSTANTS.TREE_STATE, treeJson);
+      console.log('The following tree has beed saved: ' + treeJson);
+    };
+    //Reads tree from Local Storage
+    this.retrieve = function () {
+      var retrievedObject = localStorage.getItem(CONSTANTS.TREE_STATE);
+      return JSON.parse(retrievedObject);
+    };
+    //Deletes tree from Local Storage
+    this.delete = function () {
+      localStorage.removeItem(CONSTANTS.TREE_STATE);
+    };
+    //Checks if tree is saved in Local Storage
+    this.isTreeSaved = function () {
+      if (localStorage.getItem(CONSTANTS.TREE_STATE) === null) {
+        return false;
+      }
+      return true;
     };
 }]);
--- app/scripts/services/TreeService.js
+++ app/scripts/services/TreeService.js
@@ -1,31 +1,29 @@
 'use strict';
 
-angular.module( 'dynamic-tree-recursive' ).factory('treeService', function() {
+angular.module( 'dynamic-tree-recursive' ).service('treeService', function() {
 
     //id for new node for recursive solution
     var nodeId = 0;
 
-    return {
-        //Used for solution
-        removeNodeById: function ( arr, id ) {
-            var result = null;
+    //Used for solution
+    this.removeNodeById = function ( arr, id ) {
+      var result = null;
 
-            for(var i = 0; result === null && i < arr.length; i++){
-                if(arr[i].id === id) {
-                    result = arr[i].name;
-                    arr.splice(i, 1);
-                } else if ( arr[i].nodes.length !== 0 ) {
-                    //recursive call of function itself
-                    result = this.removeNodeById (arr[i].nodes, id);
-                }
-            }
-            return result;
-        },
-
-        //Generates id for new node for recursive solution
-        generateNodeId: function() {
-            nodeId = nodeId + 1;
-            return nodeId;
+      for(var i = 0; result === null && i < arr.length; i++){
+        if(arr[i].id === id) {
+          result = arr[i].name;
+          arr.splice(i, 1);
+        } else if ( arr[i].nodes.length !== 0 ) {
+          //recursive call of function itself
+          result = this.removeNodeById (arr[i].nodes, id);
         }
+      }
+      return result;
+    };
+
+    //Generates id for new node for recursive solution
+    this.generateNodeId = function() {
+      nodeId = nodeId + 1;
+      return nodeId;
     };
 });
